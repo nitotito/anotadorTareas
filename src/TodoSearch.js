831bf998fcaeaefe9e2e5a5e84ae753e40412f65
nitotito <amarant2010@hotmail.com>
2024-07-10 19:56:54 -0300

    primer commit

---
+++ src/TodoSearch.js
@@ -0,0 +1,21 @@
+import React  from 'react';
+import './TodoSearch.css';
+
+function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log('Buscando todos de ' + searchValue);
+  return (
+    <input
+      placeholder="Cortar cebolla"
+      className="TodoSearch"
+      value={searchValue}
+      onChange={ (event) => {
+        setSearchValue(event.target.value);
+        
+      }}
+    />
+  );
+}
+
+export { TodoSearch };
